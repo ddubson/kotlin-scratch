878308a5153db3ef6f8d7eb50e0fccb314487aa2
Dmitriy Dubson <d.dubson@gmail.com>
2018-07-24 13:40:50 -0400

    Add vararg example

--- src/functions.kt
+++ src/functions.kt
@@ -8,8 +8,19 @@ fun throwingExceptions(): Nothing {
 
 fun add(x: Int = 2, y: Int = 6) = x + y
 
+// vararg -> variable number of strings passed in
+fun printSomeStrings(vararg someStrings: String) {
+    // Using the spread operator (*)
+    reallyPrint(*someStrings)
+}
+
+fun reallyPrint(vararg strings: String) {
+    strings.forEach { println(it) }
+}
+
 fun main(args: Array<String>) {
     hello()
     println(add(x = 1, y = 2))
     println(add())
+    printSomeStrings("1", "2", "3")
 }
