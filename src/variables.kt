2c4c7f92d028ad51135eb2d475ce3f22da954fc0
Dmitriy Dubson <d.dubson@gmail.com>
2018-07-24 09:10:39 -0400

    Add vars

---
+++ src/variables.kt
@@ -0,0 +1,31 @@
+// Mutable vars
+
+var streetNumber: Int = 0
+var streetName: String = "Highstreet"
+
+// Type inference
+var streetName2 = "Highstreet"
+
+// Immutable vars
+
+val zip = "10234"
+
+val myLong = 10L
+val myFloat = 20f
+val myHex = 0x2F
+val myBinary = 0xb01
+
+val myChar = 'A'
+val myString = "Mine"
+val escapeChars = "A new line\nHello"
+
+val multilineString = """
+    Hello!
+    Another line
+    Yay!
+""".trimIndent()
+
+val years = 10
+
+// String interpolation
+val message = "A decade is $years years"
