82e6f262dbe0184d1a6ddc87ab3ae800acfa6986
Dmitriy Dubson <d.dubson@gmail.com>
2018-07-25 09:17:13 -0400

    Add more stuff

--- src/classes.kt
+++ src/classes.kt
@@ -1,13 +1,39 @@
+import java.util.*
+
 class Customer
 
-class AnotherCustomer(val id: Int, var name: String) {
+class AnotherCustomer(val id: Int, var name: String, val yearOfBirth: Int) {
     init {
         name = name.toUpperCase()
+    }
+
+    constructor(email: String) : this(1, "Name", 1945) {
+
+    }
+
+    // getter
+    val age: Int
+        get() = Calendar.getInstance().get(Calendar.YEAR) - yearOfBirth
+
+    // setter
+    var ssn: String = "SN1"
+        set(value) {
+            if(!value.startsWith("SN")) {
+                throw IllegalArgumentException("Should start with SN")
+            }
+            // kotlin concept of `field` that you are setting, in this case it is `ssn`
+            field = value
+        }
+
+    fun customerAsString(): String {
+        return "Id: $id - Name: $name"
     }
 }
 
 fun main(args: Array<String>) {
-    val cust = AnotherCustomer(1, "Hello")
+    val cust = AnotherCustomer(1, "Hello", 1985)
 
+    println(cust.customerAsString())
     println(cust.name)
+    println(cust.age)
 }
