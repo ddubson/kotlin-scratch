51e1de8664462581dd78aa1e4667bb9cb981b0ef
Dmitriy Dubson <d.dubson@gmail.com>
2018-07-28 20:40:22 -0400

    longest common subsequence start

---
+++ src/longestCommonSubsequence.kt
@@ -0,0 +1,10 @@
+fun lcs(string1: String, string2: String): String {
+    val bothEmpty = string1.isEmpty() && string2.isEmpty()
+    val stringsAreNotEqual = string1 != string2
+
+    return if(bothEmpty || stringsAreNotEqual) {
+        ""
+    } else {
+        string1
+    }
+}
---
+++ test/longestCommonSubsequenceTest.kt
@@ -0,0 +1,60 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class LongestCommonSubsequence : Spek({
+    describe("provided two equal length strings") {
+        describe("of length 0") {
+            val string1 = ""
+            val string2 = ""
+
+            it("reports LCS as empty") {
+                assertThat(lcs(string1, string2)).isEqualTo("")
+            }
+        }
+
+        describe("of length 1") {
+            describe("and has no letters in common") {
+                val string1 = "A"
+                val string2 = "B"
+
+                it("reports LCS as empty") {
+                    assertThat(lcs(string1, string2)).isEqualTo("")
+                }
+            }
+
+            describe("and has letters in common") {
+                val string1 = "A"
+                val string2 = "A"
+
+                it("reports LCS as 'A'") {
+                    assertThat(lcs(string1, string2)).isEqualTo("A")
+                }
+            }
+        }
+
+        describe("of length more than 1") {
+            describe("and has no letters in common") {
+                val string1 = "AB"
+                val string2 = "CD"
+
+                it("reports LCS as empty") {
+                    assertThat(lcs(string1, string2)).isEqualTo("")
+                }
+            }
+
+            describe("and has letters in common") {
+                val listOfPairs = listOf(
+                        Pair("AB", "AB"),
+                        Pair("ABC", "AB"))
+
+                listOfPairs.forEach {
+                    it("reports LCS as AB") {
+                        assertThat(lcs(it.first, it.second)).isEqualTo("AB")
+                    }
+                }
+            }
+        }
+    }
+})
