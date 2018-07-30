e79c53a7cced70b45060b2067e166f325345af4f
Dmitriy Dubson <d.dubson@gmail.com>
2018-07-30 09:02:27 -0400

    Add functional.kt

---
+++ src/functional.kt
@@ -0,0 +1,20 @@
+fun sum(x: Int, y: Int): Int = x + y
+
+fun operation(x: Int, y: Int, op: (Int, Int) -> Int): Int = op(x, y)
+
+fun unaryOperation(x: Int, op: (Int) -> Int): Int = op(x)
+
+fun main(args: Array<String>) {
+    // ::sum is a function reference
+    println(operation(1, 2, ::sum))
+
+    // Lambda as variable
+    val addOp: (x: Int, y: Int) -> Int = { x, y -> x + y }
+    println(operation(1, 2, addOp))
+
+    // Inline lambda expression as third parameter, moved out of param list because its last in list
+    println(operation(1, 2) { x, y -> x + y })
+
+    // Single param lambdas can use the word `it` to denote the param passed in
+    println(unaryOperation(4) { it * 6 })
+}
