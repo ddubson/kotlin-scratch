31bb8c2990f43c9c48c44470e6726602669617d8
Dmitriy Dubson <d.dubson@gmail.com>
2018-08-08 15:03:23 -0400

    --

--- test/problems/happyLadybugsTest.kt
+++ test/problems/happyLadybugsTest.kt
@@ -11,24 +11,43 @@ object Answer {
 }
 
 fun happyLadybugs(input: String): String {
-    return Answer.NO
+    // Count number of instances of a letter
+    // If all counts are even, then return yes
+
+    countOccurrences(input).forEach {
+        if(it.value % 2 != 0) {
+            return Answer.NO
+        }
+    }
+
+    return Answer.YES
 }
 
-internal class HappyLadybugsTest: Spek({
+fun countOccurrences(input: String) = input.toList().asSequence().groupingBy { it }.eachCount()
+
+internal class HappyLadybugsTest : Spek({
     describe("when there are no empty spaces") {
         describe("and there are non-adjacent colors") {
             val inputString = "RBY"
-            
+
             it("should return 'NO'") {
                 assertThat(happyLadybugs(inputString)).isEqualTo(Answer.NO)
             }
         }
-        
-        describe("and there are colors that are alone") {
+
+        describe("and there are colors that are alone in the string") {
             val inputString = "YXX"
 
             it("should return 'NO'") {
                 assertThat(happyLadybugs(inputString)).isEqualTo(Answer.NO)
+            }
+        }
+
+        describe("and there are colors that are all satisfied") {
+            val inputString = "YYXX"
+
+            it("should return 'YES'") {
+                assertThat(happyLadybugs(inputString)).isEqualTo(Answer.YES)
             }
         }
     }
