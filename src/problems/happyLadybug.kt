84e98502f253c771f9c1889f3dc04c580500d56e
Dmitriy Dubson <d.dubson@gmail.com>
2018-08-10 19:19:18 -0400

    WIP

---
+++ src/problems/happyLadybug.kt
@@ -0,0 +1,79 @@
+package problems
+
+object Answer {
+    const val NO = "NO"
+    const val YES = "YES"
+}
+
+fun happyLadybugs(input: String): String {
+    if (input.isEmpty()) {
+        return Answer.NO
+    }
+
+    var prev = '!'
+    val mutableInput = input.asSequence().toMutableList()
+
+    for ((index, c) in input.withIndex()) {
+        if (c == '_') {
+            continue
+        }
+
+        if (index + 1 == input.length) {
+            break
+        }
+
+        if (mutableInput[index + 1] != c) {
+            // seek forward to underscore
+            val posOfNextUnderscore = findNextUnderscore(input)
+            if (posOfNextUnderscore == -1) {
+                if(prev == c) {
+                    continue
+                } else {
+                    return Answer.NO
+                }
+            } else {
+                // swap underscore with next char
+                val nextChar = mutableInput[index + 1]
+                mutableInput[index + 1] = '_'
+                mutableInput[posOfNextUnderscore] = nextChar
+            }
+
+            val posOfNextChar = findNextChar(input.substring(index + 1), c)
+
+            if (posOfNextChar == -1 && prev != c) {
+                return Answer.NO
+            } else {
+                // swap underscore with next char
+                mutableInput[index + 1] = mutableInput[posOfNextChar]
+                mutableInput[posOfNextChar] = '_'
+            }
+
+            prev = c
+        } else {
+            prev = c
+        }
+    }
+
+    return if (checkIfHappy(mutableInput)) "YES" else "NO"
+}
+
+fun findNextUnderscore(input: String): Int = input.indexOf('_')
+
+fun findNextChar(input: String, c: Char): Int = input.indexOf(c)
+
+fun checkIfHappy(input: MutableList<Char>): Boolean {
+    var memoryChar = '!'
+    input.forEachIndexed { index, c ->
+        if (index + 1 == input.size) {
+            return@forEachIndexed
+        }
+
+        if (c == input[index + 1] || memoryChar == c) {
+            memoryChar = c
+        } else {
+            return false
+        }
+    }
+
+    return true
+}
--- test/problems/happyLadybugsTest.kt
+++ test/problems/happyLadybugsTest.kt
@@ -1,75 +1,10 @@
 package problems
 
-import com.sun.org.apache.xpath.internal.operations.Bool
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-
-object Answer {
-    val NO = "NO"
-    val YES = "YES"
-}
-
-fun happyLadybugs(input: String): String {
-    if (input.isEmpty()) {
-        return Answer.NO
-    }
-
-    var prev = '!'
-    val mutableInput = input.asSequence().toMutableList()
-
-    for ((index, c) in input.withIndex()) {
-        if (c == '_') {
-            continue
-        }
-
-        if (mutableInput[index + 1] != c) {
-            // seek forward to underscore
-            val posOfNextUnderscore = findNextUnderscore()
-            if (posOfNextUnderscore == -1 && prev != c) {
-                return Answer.NO
-            } else {
-                // swap underscore with next char
-                val nextChar = mutableInput[index + 1]
-                mutableInput[index + 1] = '_'
-                mutableInput[posOfNextUnderscore] = nextChar
-            }
-
-            val posOfNextChar = findNextChar(c)
-
-            if (posOfNextChar == -1 && prev != c) {
-                return Answer.NO
-            } else {
-                // swap underscore with next char
-                mutableInput[index + 1] = mutableInput[posOfNextChar]
-                mutableInput[posOfNextChar] = '_'
-            }
-        } else {
-            prev = c
-        }
-    }
-
-    return if (checkIfHappy(mutableInput)) "YES" else "NO"
-}
-
-fun checkIfHappy(input: MutableList<Char>): Boolean {
-    var memoryChar = '!'
-    input.forEachIndexed { index, c ->
-        if (index + 1 == input.size) {
-            return@forEachIndexed
-        }
-
-        if (c == input[index + 1] || memoryChar == c) {
-            memoryChar = c
-        } else {
-            return false
-        }
-    }
-
-    return true
-}
 
 internal class HappyLadybugsTest : Spek({
     describe("when there are no empty spaces") {
